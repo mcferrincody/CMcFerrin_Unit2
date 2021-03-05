526d43c4e45683131f3e21214747d514d4c7625a
mcferrincody <61605145+mcferrincody@users.noreply.github.com>
2021-03-05 01:51:21 -0800

    Unit 2 Lesson B done!

---
+++ Assets/Scripts/DestroyOutofBounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutofBounds : MonoBehaviour
+{
+    private float topofScene = 40.0f;
+    private float bottomofScene = -15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topofScene)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < bottomofScene)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
