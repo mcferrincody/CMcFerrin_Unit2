70fad80f89852bb3369ba60bca89b56cd738f206
mcferrincody <61605145+mcferrincody@users.noreply.github.com>
2021-03-07 00:46:58 -0800

    Unit 2 Lesson D

--- Assets/Scripts/DestroyOutofBounds.cs
+++ Assets/Scripts/DestroyOutofBounds.cs
@@ -22,6 +22,7 @@ public class DestroyOutofBounds : MonoBehaviour
         else if(transform.position.z < bottomofScene)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
     }
 }
